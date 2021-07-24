11ab7d9f64cd1ef2e117ed3d7db402e3ec289c5e
jia <Fire_Mars_j@163.com>
2021-07-24 09:07:18 +0800

    feat:FastClonerListOf12 and FastClonerSetOf12

--- src/main/java/com/rits/cloning/FastClonerSetOf12.java
+++ src/main/java/com/rits/cloning/FastClonerSetOf12.java
@@ -2,16 +2,18 @@ package com.rits.cloning;
 
 import java.util.*;
 
-public class FastClonerSetOf12 implements IFastCloner{
+public class FastClonerSetOf12 implements IFastCloner {
     @SuppressWarnings({"unchecked", "rawtypes"})
     public Object clone(final Object t, final IDeepCloner cloner, final Map<Object, Object> clones) {
-        Set set=(Set) t;
-        Object[] a=set.toArray();
-        if (set.size()==1){
-            return Set.of(a[0]);
-        }else if (set.size()==2){
-            return Set.of(a[0],a[1]);
-        }else {
+        Set set = (Set) t;
+        Object[] a = set.toArray();
+        if (set.size() == 1) {
+            return Set.of(cloner.deepClone(a[0], clones));
+        } else if (set.size() == 2) {
+            Object o1 = cloner.deepClone(a[0], clones);
+            Object o2 = cloner.deepClone(a[1], clones);
+            return Set.of(o1, o2);
+        } else {
             return new HashSet<>();
         }
     }
