b0ff9568d5749451c444c4422721af51bd513db2
jia <Fire_Mars_j@163.com>
2021-07-24 09:07:18 +0800

    feat:FastClonerListOf12 and FastClonerSetOf12

--- src/main/java/com/rits/cloning/FastClonerListOf12.java
+++ src/main/java/com/rits/cloning/FastClonerListOf12.java
@@ -8,11 +8,13 @@ public class FastClonerListOf12 implements IFastCloner {
     @SuppressWarnings({"unchecked", "rawtypes"})
     public Object clone(final Object t, final IDeepCloner cloner, final Map<Object, Object> clones) {
         List al = (List) t;
-        if (al.size()==1){
-            return List.of(al.get(0));
-        }else if (al.size()==2){
-            return List.of(al.get(0),al.get(1));
-        }else {
+        if (al.size() == 1) {
+            return List.of(cloner.deepClone(al.get(0), clones));
+        } else if (al.size() == 2) {
+            Object o1 = cloner.deepClone(al.get(0), clones);
+            Object o2 = cloner.deepClone(al.get(1), clones);
+            return List.of(o1, o2);
+        } else {
             return new ArrayList<>();
         }
     }
